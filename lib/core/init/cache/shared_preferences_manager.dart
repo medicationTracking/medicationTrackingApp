9414a3bf67ade49f3c0ab8ceb734b796b7be715b
halil <halilbalci878@gmail.com>
2021-04-07 23:59:57 +0300

    shared preferences manager working properly.

---
+++ lib/core/init/cache/shared_preferences_manager.dart
@@ -0,0 +1,38 @@
+import 'package:medication_app_v0/core/constants/enums/shared_preferences_enum.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesManager {
+  static final SharedPreferencesManager _instance =
+      SharedPreferencesManager._init();
+
+  SharedPreferences _preferences;
+  static SharedPreferencesManager get instance => _instance;
+
+  SharedPreferencesManager._init() {
+    SharedPreferences.getInstance().then((value) => _preferences = value);
+  }
+
+  static Future preferencesInit() async {
+    if (instance._preferences == null) {
+      instance._preferences = await SharedPreferences.getInstance();
+    }
+  }
+
+  Future<void> clearAll() async {
+    await _preferences.clear();
+  }
+
+  Future<void> setStringValue(SharedPreferencesKey key, String value) async {
+    await _preferences.setString(key.toString(), value);
+  }
+
+  Future<void> setBoolValue(SharedPreferencesKey key, bool value) async {
+    await _preferences.setBool(key.toString(), value);
+  }
+
+  String getStringValue(SharedPreferencesKey key) =>
+      _preferences.getString(key.toString()) ?? '';
+
+  bool getBoolValue(SharedPreferencesKey key) =>
+      _preferences.getBool(key.toString()) ?? false;
+}
